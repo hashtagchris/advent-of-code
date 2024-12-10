3984645d4b19ceb4e3211e3c8bfd150f352acdec
Chris Sidi <hashtagchris@github.com>
2024-12-10 12:09:03 -0500

    Use regex for char splitting instead

--- 2024/day/10/part1.ts
+++ 2024/day/10/part1.ts
@@ -1,7 +1,10 @@
 export function evaluate(mapText: string): number {
   // convert the text to a 2D array of integers.
   const map: number[][] = mapText.split(/\n/g)
-    .map((line) => line.split("").map((v) => parseInt(v)));
+    // ignore blank lines
+    .filter(Boolean)
+    // Another option for parsing the string's Unicode code points: [...line].map((v) => parseInt(v))
+    .map((line) => line.split(/\s?/).map((v) => parseInt(v)));
 
   let totalScore = 0;
   for (let r = 0; r < map.length; r++) {
@@ -25,7 +28,8 @@ function workingPaths(
   trailEnds: Set<string>,
 ) {
   if (map[r][c] === 9) {
-    // I'm adding the string representation of coordinates to the Set because you can't customize Object equality.
+    // I'm adding the string representation of coordinates to the Set because you can't override the comparison of objects.
+    // new Set([{ a: 1 }, { a: 1 }]).size returns 2.
     // See https://stackoverflow.com/questions/29759480/how-to-customize-object-equality-for-javascript-set
     trailEnds.add(JSON.stringify({ r, c }));
     return;
