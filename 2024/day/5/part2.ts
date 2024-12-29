e522a3a8760c4faef600fe5ff4c3c885f1f69e16
Chris Sidi <hashtagchris@github.com>
2024-12-29 18:55:29 -0500

    Use Array.prototype.toSorted

--- 2024/day/5/part2.ts
+++ 2024/day/5/part2.ts
@@ -6,40 +6,44 @@ export function evaluate(input: string): number {
   const firstLines = lines.slice(0, blankIdx);
   const secondLines = lines.slice(blankIdx + 1);
 
-  const orderPairs = firstLines.map((line) => line.split("|"));
-  const updates = secondLines.map((line) => {
-    const pages = line.split(",");
+  // create a map to support sorting pages
+  const orderMap: { [key: string]: { [key: string]: number } } = {};
 
+  for (const orderPair of firstLines.map((line) => line.split("|"))) {
+    orderMap[orderPair[0]] ||= {};
+    orderMap[orderPair[1]] ||= {};
+
+    // the first page comes before the second page. Given 43|66, our compare function
+    // should return -1 for (43, 66), and 1 for (66, 43).
+    orderMap[orderPair[0]][orderPair[1]] = -1;
+    orderMap[orderPair[1]][orderPair[0]] = 1;
+  }
+
+  const updates = secondLines.map((line) => {
     return {
-      pages,
+      pages: line.split(","),
+      sortedPages: <string[]> [],
       validInitially: true,
     };
   });
 
-  let stable: boolean;
-  do {
-    stable = true;
-    for (const orderPair of orderPairs) {
-      for (const update of updates) {
-        const firstIdx = update.pages.indexOf(orderPair[0]);
-        const secondIdx = update.pages.indexOf(orderPair[1]);
+  for (const update of updates) {
+    // sort the update's pages using the order map
+    update.sortedPages = update.pages.toSorted((a, b) => orderMap[a]?.[b] || 0);
 
-        if (secondIdx < firstIdx && secondIdx > -1) {
-          update.validInitially = false;
-          update.pages[firstIdx] = orderPair[1];
-          update.pages[secondIdx] = orderPair[0];
-          stable = false;
-        }
-      }
-    }
-  } while (!stable);
+    // if any pages were reordered, set validInitially to false
+    update.validInitially = update.sortedPages.every((v, i) =>
+      v === update.pages[i]
+    );
+  }
 
   const middleSum = updates
     // ignore the page orders that were valid initially
     .filter((u) => !u.validInitially)
     // add up the middle page numbers
     .reduce(
-      (acc, cv) => acc + parseInt(cv.pages[(cv.pages.length - 1) / 2]),
+      (acc, cv) =>
+        acc + parseInt(cv.sortedPages[(cv.sortedPages.length - 1) / 2]),
       0,
     );
 
